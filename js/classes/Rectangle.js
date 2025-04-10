88d52db7ce4c264a0f5450a8a584ca345b82ea95
Aurelien <ademaille@gmail.fr>
2025-04-10 13:58:50 +0200

    Ajout du constructor dans Rectangle.js

--- js/classes/Rectangle.js
+++ js/classes/Rectangle.js
@@ -1,1 +1,14 @@
 // Vous mettrez ici le code de la classe Rectangle
+
+class Rectangle {
+
+    #length;
+    #width;
+
+    constructor(length, width) {
+        this.#length = length
+        this.#width = width
+    }
+}
+
+    
